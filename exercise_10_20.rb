a5b4b2ff8c5340362bbcf243f031d65fabf87898
Remington <remingtonnberg@gmail.com>
2020-10-21 15:38:34 -0500

    10/20 exercise 3

---
+++ exercise_10_20.rb
@@ -0,0 +1,9 @@
+puts "Greetings! What is your year of origin?"
+origin = gets.chomp.to_i
+if origin < 1900
+  puts "That's the past!"
+elsif origin > 1900 && origin < 2020
+  puts "That's the present!"
+else
+  puts "That's the future!"
+end
